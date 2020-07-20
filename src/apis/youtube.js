cbf98539c163efe3f6743a159d87358a4789f0cf
Darren Suroviec <dcsuroviec@gmail.com>
2020-07-20 09:24:37 -0700

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //reminder that there are only a limited number of requests allowed. if run out, need to make a new key at youtube v3
-const KEY = "AIzaSyCv7sh664Fy4dYRgWaf0oAwKXkeLQ_AQgA";
+const KEY = "AIzaSyARuoptpJo-lTkhuigRe4C0qr74y0LpSLk";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
